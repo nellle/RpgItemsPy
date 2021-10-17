e13407f8d822346a70d22c1bc8a4639a4721786f
Урядов Алексей <63245550+LedinecMing@users.noreply.github.com>
2021-10-18 00:21:53 +0400

    Всегда пожалуйста!

--- Items.py
+++ Items.py
@@ -1,67 +1,60 @@
 from random import randint
+import typing
+
+DamageInfo = typing.Sequence[int]
+ArgsChecker = typing.Dict[str, typing.Sequence]
+
+NORMAL_DAMAGE_LENGTH = 2
+NORMAL_DAMAGE_TYPE = int
 
 
-class Weapon:
-    def __init__(self, damage: list = [1, 2], name: str = 'Sword'):
-         self.damage = damage
-         self.name = name 
-         self.__check()
-
-    def get_dmg(self, protect = 0):
-        dmg = float(randint(self.damage[0], self.damage[1]))
-        dmg -= dmg*0.01*protect
-        return round(dmg, 1)
-
-    
+def real_damage(damage: float, defence: float) -> int:
+    return round(damage-(damage*0.01*defence))
 
 
-    
+class Item:
+    arg_types: ArgsChecker = {"name": [str, "raise ValueError(f'Name must be str, got {arg}({type(arg)})')"]}
+
+    @staticmethod
+    def check_args(args: typing.Dict[str, typing.Any], arg_types: ArgsChecker):
+        for (arg, normalType) in arg_types.items():
+            if not isinstance(args[arg], normalType[0]):
+                exec(normalType[1], {"ValueError": ValueError, "arg": arg})
+        return True
 
 
+class Weapon(Item):
+    arg_types: ArgsChecker = {"damage": [typing.Sequence, "raise ValueError(f'Damage is not subclass of typing.Sequence type, got {arg}({type(arg)})')"],
+                              "name": [str, "raise ValueError(f'Wrong type for name str or bytes excepted, got {arg}({type(arg)})')"]}
+
+    def __init__(self, damage: DamageInfo, name: typing.AnyStr):
+        if self.check_args({"damage": damage, "name": name}, self.arg_types):
+            self._damage = damage
+            self._name = name
+    @property
+    def damage(self):
+        return float(randint(self._damage[0], self._damage[1]))
+
+    @property
+    def name(self):
+        return self._name
+
+    def check_args(self, args: typing.Dict[str, typing.Any], arg_types: ArgsChecker):
+        super().check_args(args, self.arg_types)
+        if any(not isinstance(damageConfig, int) for damageConfig in args["damage"]):
+            raise ValueError(f"Wrong types in damage: {type(args['damage'])}")
+        elif len(args["damage"]) != 2:
+            raise ValueError(f"Damage length must be 2, got {len(args['damage'])}")
+        return True
+
+class Armor(Item):
+    arg_types: ArgsChecker = {"name": [str, "raise ValueError(f'Wrong type for name str or bytes excepted, got {arg}({type(arg)})')"],
+                              "defence": [int, 'raise ValueError(f"Defence must be int, got {arg}({type(arg)})")']}
+
+    def __init__(self, defence: int, name: typing.AnyStr):
+        if super().check_args({"defence": defence, "defence": name}, self.arg_types):
+            self._defence = defence
+            self._name = name
 
 
-
-
-
-
-
-    def __check(self):
-        if type(self.damage) != list:
-            raise ValueError("Damage not list!")
-        if len(self.damage) > 2:
-            raise ValueError("Damage list greater than 2")
-        if type(self.damage[0]) != int or type(self.damage[1]) != int:
-            raise ValueError("Damage not int") 
-        if type(self.name) != str:
-            raise ValueError("Name not str!")
-        if self.damage[0] > self.damage[1]:
-            raise ValueError("The first number is greater than the second!")
-
-class Armor():
-    def __init__(self, protect: int = 0, name: str = 'armor'):
-        self.protect = protect
-        self.name = name
-        self.__check()
-
-
-
-
-
-
-    def __check(self):
-        if type(self.protect) != int:
-            raise ValueError("protect not int!")
-        if type(self.name) != str:
-            raise ValueError("name not str!")
-        
-
-class Item():
-    def __init__(self, name: str = 'item'):
-        self.name = name
-        self.__check()
-
-    def __check(self):
-        if type(self.name) != str:
-            raise ValueError("name not str!")
-
-    
+Weapon([1, 2], "stuka")
